bfd333855c263f7e5cf80901448f4c420895f4b7
codeWithGodstime <specialgodstym@gmail.com>
2025-10-11 23:17:06 +0100

    sks

--- drf_starter/settings/production.py
+++ drf_starter/settings/production.py
@@ -10,7 +10,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': env("DATABASE_NAME"),
-        'USER': env("POSTGRES_USER"),
+        'USER': env("DATABASE_USER"),
         'PASSWORD': env("DATABASE_PASSWORD"),
         'HOST': env("DATABASE_HOST"),
         'PORT': env("DATABASE_PORT")
