bbd7a82b27f107969af1f2531716a6b35006abfa
codeWithGodstime <specialgodstym@gmail.com>
2025-10-25 12:30:12 +0100

    update

--- drf_starter/settings/production.py
+++ drf_starter/settings/production.py
@@ -14,7 +14,7 @@ DATABASES = {
     "default": dj_database_url.parse(
         env("DATABASE_URL"),
         conn_max_age=600, 
-        ssl_require=True, 
+        ssl_require=False, #
     )
 }
 # CORS
